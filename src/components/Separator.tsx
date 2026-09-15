d15f77a820cb437b34c3862bcb0f3c5d82ca911c
Imtiaz Ali <imtiazali17314@gmail.com>
2026-09-15 12:10:53 +0500

    feat: add ProductItem and Separator components for product display

---
+++ src/components/Separator.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Separator = () => {
+  return <View style={styles.separator}></View>;
+};
+
+export default Separator;
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 1,
+    backgroundColor: '#CAD5E2',
+    marginVertical: 10,
+  },
+});
